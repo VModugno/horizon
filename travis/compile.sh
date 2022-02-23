362b5246fb7d05fd80baca0b72570f35ef1acf65
FrancescoRuscelli <francescoruscelli24@gmail.com>
2022-02-23 14:44:36 +0100

    again

--- travis/compile.sh
+++ travis/compile.sh
@@ -1,5 +1,7 @@
 if [ '$DISTRIBUTION' = python ]; then
-    ./python_compile.sh
+    ./python_compile.sh;
+fi
 
 if [ '$DISTRIBUTION' = python ]; then
-    ./conda_compile.sh
+    ./conda_compile.sh;
+fi
--- travis/test.sh
+++ travis/test.sh
@@ -2,3 +2,4 @@
 
 if [ '$DISTRIBUTION' = python ]; then
     python3 horizon/tests/test_get_set.py
+fi
