8b1c6debbff15c70b3c699c381f4896efdd29e6d
FrancescoRuscelli <francescoruscelli24@gmail.com>
2022-02-24 18:10:16 +0100

    strange matrix travis

--- travis/conda_init.sh
+++ travis/conda_init.sh
@@ -3,6 +3,7 @@
 curl -L -O https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh
 printf '\nyes\n\nyes\n' | bash Mambaforge-$(uname)-$(uname -m).sh
 
-export PATH /home/daniele/mambaforge/bin:$PATH
+export PATH=$PWD/mambaforge/bin:$PATH
 
+echo $PWD/mambaforge/bin:$PATH
 yes Y | mamba env create -f environment.yml
