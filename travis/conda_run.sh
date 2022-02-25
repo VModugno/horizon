eeea912b369faddfd2242ce0e6c5d8acb9d9caba
FrancescoRuscelli <francescoruscelli24@gmail.com>
2022-02-25 18:32:37 +0100

    asd

--- travis/conda_run.sh
+++ travis/conda_run.sh
@@ -23,7 +23,7 @@ echo -e "building package $PACKAGE_NAME .."
 
 # building the casadi_kin_dyn package
 # using the following command instead of "conda-build" because of error "DependencyNeedsBuildingError: Unsatisfiable dependencies for platform linux-64"
-conda build -c conda-forge -c francesco_ruscelli -c robostack $PACKAGE_NAME
+conda build -c conda-forge -c francesco_ruscelli -c robostack .
 
 echo -e "done."
 
