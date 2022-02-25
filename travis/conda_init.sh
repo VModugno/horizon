165bd3f76998d81109ca2f477211c238fc8ffa52
FrancescoRuscelli <francescoruscelli24@gmail.com>
2022-02-25 10:40:36 +0100

    matrix trial travis

--- travis/conda_init.sh
+++ travis/conda_init.sh
@@ -5,5 +5,8 @@ printf '\nyes\n\nyes\n' | bash Mambaforge-$(uname)-$(uname -m).sh
 
 export PATH=$PWD/mambaforge/bin:$PATH
 
-echo $PWD/mambaforge/bin:$PATH
+# re-activate shell
+source ~/.bashrc
+
+# create environment for conda
 yes Y | mamba env create -f environment.yml
