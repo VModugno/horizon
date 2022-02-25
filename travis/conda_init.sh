9635105b4413d1221a58ce4482d463625334b6b1
FrancescoRuscelli <francescoruscelli24@gmail.com>
2022-02-25 15:16:41 +0100

    random tries

--- travis/conda_init.sh
+++ travis/conda_init.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-set -ev
+# -i stands for interactive mode: required to make effective source ~/.bashrc
+set -v
 
 curl -L -O https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh
 printf '\nyes\n\nyes\n' | bash Mambaforge-$(uname)-$(uname -m).sh
+
+
+# re-activate shell
+export PATH=$PWD/mambaforge/bin:$PATH
+source ~/.bashrc && yes Y | mamba env create -f environment.yml
+
+# create environment for conda
