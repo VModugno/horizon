d65263eb0b9d10c7b95d4893dafb5457202cbcea
FrancescoRuscelli <francescoruscelli24@gmail.com>
2022-02-25 12:28:32 +0100

    fix

--- travis/conda_init.sh
+++ travis/conda_init.sh
@@ -4,10 +4,12 @@ set -ev
 curl -L -O https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh
 printf '\nyes\n\nyes\n' | bash Mambaforge-$(uname)-$(uname -m).sh
 
-export PATH=$PWD/mambaforge/bin:$PATH
 
+
+echo "before sourcing: $PATH"
 # re-activate shell
 source ~/.bashrc
-
+export PATH=$PWD/mambaforge/bin:$PATH
+echo "after sourcing: $PATH"
 # create environment for conda
 yes Y | mamba env create -f environment.yml
