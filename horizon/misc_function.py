2543c62ebac02fc2753874d499540713a713df7e
FrancescoRuscelli <francescoruscelli24@gmail.com>
2022-04-06 18:29:19 +0200

    fixed function bug

--- horizon/misc_function.py
+++ horizon/misc_function.py
@@ -1,4 +1,5 @@
 import numpy as np
+import warnings
 
 def unravelElements(elements):
     if isinstance(elements, int):
@@ -38,30 +39,16 @@ def checkNodes(nodes, nodes_array=None):
         checked_nodes = nodes_vec
     else:
         # get from nodes only the nodes active in nodes_array
+        # example: nodes_array = [0 0 1 0 1 1 0 0]
+        #                nodes = [2, 3, 4]
+        #       1. get from nodes array only the elements at position [2, 3, 4]  --> [1 0 1]
+        #       2. mask 'nodes' with [1 0 1] --> [2, 4]
         checked_nodes = np.ma.masked_array(nodes_vec, mask=np.logical_not(nodes_array[nodes_vec])).compressed()
 
-    # if hasattr(nodes, "__iter__") and not isinstance(nodes, str):
-    #     if nodes_array is None:
-    #         checked_nodes = nodes
-    #     else:
-    #
-    #         checked_nodes = nodes_array[nodes]
-    #         np.array()
-    #
-    #         print(checked_nodes)
-    #         exit()
-    # elif isinstance(nodes, (int, np.integer)):
-    #     if nodes_array is None:
-    #         checked_nodes = [nodes]
-    #     else:
-    #         checked_nodes = [nodes] if nodes in nodes_array else []
-    # else:
-    #     raise Exception('Type {} is not supported to specify nodes.'.format(type(nodes)))
-    #
-    # if checked_nodes is None:
-    #     raise Exception(f'Invalid nodes inserted: {nodes}')
-    # # todo ADD WARNING if node (or nodes) are NOT present in own nodes.
-    # #  (basically, when setting bounds for some node, it is left out because the var/fun does not exist in that node
+        if checked_nodes.size != nodes_vec.size:
+            wrong_nodes = np.ma.masked_array(nodes_vec, mask=nodes_array[nodes_vec]).compressed()
+            warnings.warn(f'Element requested is not defined on node: {wrong_nodes}.')
+
     return checked_nodes
 
 def checkValueEntry(val):
