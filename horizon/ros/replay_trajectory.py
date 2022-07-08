47b43bf5fc1e4fea9cd427fc22e7691dbcb25146
Arturo Laurenzi <arturo.laurenzi@iit.it>
2022-07-08 11:06:48 +0200

    selectable base link

--- horizon/ros/replay_trajectory.py
+++ horizon/ros/replay_trajectory.py
@@ -135,7 +135,7 @@ class replay_trajectory:
         '''
         self.slow_down_rate = 1./slow_down_factor
 
-    def publish_joints(self, qk, is_floating_base=True):
+    def publish_joints(self, qk, is_floating_base=True, base_link='base_link'):
         joint_state_pub = JointState()
         joint_state_pub.header = Header()
         joint_state_pub.name = self.joint_list
@@ -149,7 +149,7 @@ class replay_trajectory:
             
             m = geometry_msgs.msg.TransformStamped()
             m.header.frame_id = 'world'
-            m.child_frame_id = 'base_link'
+            m.child_frame_id = base_link
             m.transform.translation.x = qk[0]
             m.transform.translation.y = qk[1]
             m.transform.translation.z = qk[2]
