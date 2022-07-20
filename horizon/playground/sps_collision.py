1e808d299546d00f2199e923d24b6deff44b1b22
EdoardoRomiti <edoardo.romiti@iit.it>
2022-07-20 16:29:53 +0200

    add temporary example

---
+++ horizon/playground/sps_collision.py
@@ -0,0 +1,164 @@
+from horizon import problem
+from horizon.utils import utils, kin_dyn, plotter, mat_storer, collision
+from casadi_kin_dyn import pycasadi_kin_dyn as cas_kin_dyn
+from horizon.solvers import solver
+from horizon.transcriptions.transcriptor import Transcriptor
+from horizon.ros.replay_trajectory import replay_trajectory
+import os, time
+import numpy as np
+import casadi as cs
+import rospkg
+
+from srdfdom.srdf import SRDF
+
+# options
+solver_type = 'ilqr'
+transcription_method = 'multiple_shooting'
+transcription_opts = dict(integrator='RK4')
+tf = 10.0
+n_nodes = 100
+dt = tf / n_nodes
+
+# load URDF
+rospack = rospkg.RosPack()
+pkgpath = rospack.get_path('mecademic_description')
+# pkgpath = rospack.get_path('ModularBot_5DOF')
+urdfpath = os.path.join(pkgpath, 'urdf', 'meca_500_r3_capsule.urdf.xacro')
+srdfpath = os.path.join(pkgpath, 'srdf', 'meca_500_r3_capsule.srdf')
+
+urdf = open(urdfpath, 'r').read()
+kindyn = cas_kin_dyn.CasadiKinDyn(urdf)
+nq = kindyn.nq()
+nv = kindyn.nv()
+
+# joint names
+joint_names = kindyn.joint_names()
+if 'universe' in joint_names: joint_names.remove('universe')
+if 'floating_base_joint' in joint_names: joint_names.remove('floating_base_joint')
+
+# define dynamics
+prb = problem.Problem(n_nodes)
+q = prb.createStateVariable('q', nq)
+q_dot = prb.createStateVariable('q_dot', nv)
+q_ddot = prb.createInputVariable('q_ddot', nv)
+x, x_dot = utils.double_integrator(q, q_dot, q_ddot)
+prb.setDynamics(x_dot)
+prb.setDt(dt)
+
+# transcription
+if solver_type != 'ilqr':
+    Transcriptor.make_method(transcription_method, prb, opts=transcription_opts)
+
+# initial state and initial guess
+q_init = np.zeros(nq)
+q.setBounds(q_init, q_init, 0)
+q_dot.setBounds(np.zeros(nv), np.zeros(nv), 0)
+q.setInitialGuess(q_init)
+
+# joint limits
+q_max = kindyn.q_max()
+q_min = kindyn.q_min()
+q.setBounds(lb=q_min, ub=q_max, nodes=range(1, n_nodes+1))
+
+# zero final velocity 
+q_dot.setBounds(lb=np.zeros(nv), ub=np.zeros(nv), nodes=n_nodes)
+
+# cartesian targets
+frame_A = 'ee_link_A'
+FK_A = kindyn.fk(frame_A)
+DFK_A = kindyn.frameVelocity(frame_A, cas_kin_dyn.CasadiKinDyn.LOCAL_WORLD_ALIGNED)
+
+frame_B = 'ee_link_B'
+FK_B = kindyn.fk(frame_B)
+DFK_B = kindyn.frameVelocity(frame_B, cas_kin_dyn.CasadiKinDyn.LOCAL_WORLD_ALIGNED)
+
+p_A = FK_A(q=q)['ee_pos']
+p_start_A = FK_A(q=q_init)['ee_pos']
+
+p_B = FK_B(q=q)['ee_pos']
+p_start_B = FK_B(q=q_init)['ee_pos']
+
+# v_A = DFK_A(q=q, qdot=q_dot)['ee_vel_linear']
+# v_B = DFK_B(q=q, qdot=q_dot)['ee_vel_linear']
+
+p_tgt_A = p_start_A + np.array([0.3, -0.1, 0.0])
+p_tgt_B = p_start_B + np.array([-0.1, +0.1, 0.0])
+
+# final node constraint velocity
+final_node=n_nodes
+prb.createFinalConstraint('ee_tgt_A', p_A - p_tgt_A)
+prb.createFinalConstraint('ee_tgt_B', p_B - p_tgt_B)
+q_dot.setBounds(lb=np.zeros(nv), ub=np.zeros(nv), nodes=final_node)
+
+# collision
+srdf = open(srdfpath, 'r').read()
+use_fcl_wrapper = False
+if use_fcl_wrapper:
+    ch = cas_kin_dyn.CollisionHandler(kd=kindyn, srdf=srdf)
+    collision_fn = ch.getDistanceFunction()
+else:
+    ch = collision.CollisionHandler(urdfstr=urdf, srdfstr=srdf, kindyn=kindyn)
+    collision_fn = ch.get_function()
+
+# collision_constr = prb.createConstraint('collision', collision_fn(q))
+# collision_constr.setBounds(lb=np.zeros(collision_fn.size1_out(0)),
+#                            ub=np.full(collision_fn.size1_out(0), np.inf))
+
+collision_sx = collision_fn(q)
+collision_barrier = cs.if_else(collision_sx > 0, 0, collision_sx)
+coll_weight = prb.createParameter('coll_weight', dim=1)
+prb.createResidual('collision', coll_weight*collision_barrier)
+coll_weight.assign(1e2)
+
+
+##### Cost function #####
+# penalize input
+prb.createIntermediateResidual('min_u', np.sqrt(1) * q_ddot)
+
+# penalize joint velocity
+prb.createIntermediateResidual('min_qdot_linear', np.sqrt(1e4) * q_dot[0])
+prb.createIntermediateResidual('min_qdot_rotary', np.sqrt(1e4) * q_dot[7])
+# prb.createIntermediateResidual('min_qdot_rotary', np.sqrt(1e4) * q_dot)
+
+# create solver
+prb_solver = solver.Solver.make_solver(
+    solver_type, 
+    prb,
+    opts={
+        'ipopt.tol': 1e-4,
+        'ipopt.max_iter': 2000,
+        'ilqr.codegen_enabled': True,
+        'ilqr.codegen_workdir': '/home/edoardo/ilqr_codegen',
+        'ilqr.enable_gn': True,
+        'ilqr.max_iter': 400,
+        }
+)
+
+try:
+    prb_solver.set_iteration_callback()
+except:
+    pass
+
+# solver
+tic = time.time()
+prb_solver.solve()
+toc = time.time()
+print('time elapsed solving:', toc - tic)
+
+solution = prb_solver.getSolutionDict()
+
+dt_sol = prb_solver.getDt()
+total_time = sum(dt_sol)
+print(f"total trajectory time: {total_time}")
+
+
+collision_values = collision_fn(solution['q']).toarray().T
+
+from matplotlib import pyplot as plt
+plt.plot(collision_values)
+plt.grid()
+plt.show()
+
+# visualize the robot in RVIZ
+joint_list = joint_names
+replay_trajectory(total_time / n_nodes, joint_list, solution['q']).replay(is_floating_base=False)
