85f37e8c9785780b9bcf76b30250f20ecb610859
FrancescoRuscelli <francescoruscelli24@gmail.com>
2022-07-21 11:46:54 +0200

    removed useless skew and transformed integrator into a sx function

--- horizon/utils/utils.py
+++ horizon/utils/utils.py
@@ -42,20 +42,20 @@ def jac(dict, var_string_list, function_string_list):
     return F, jac_map
 
 
-def skew(q):
-    """
-    Create skew matrix from vector part of quaternion
-    Args:
-        q: vector part of quaternion [qx, qy, qz]
-
-    Returns:
-        S = skew symmetric matrix built using q
-    """
-    S = cs.SX.zeros(3, 3)
-    S[0, 1] = -q[2]; S[0, 2] = q[1]
-    S[1, 0] = q[2];  S[1, 2] = -q[0]
-    S[2, 0] = -q[1]; S[2, 1] = q[0]
-    return S
+# def skew(q):
+#     """
+#     Create skew matrix from vector part of quaternion
+#     Args:
+#         q: vector part of quaternion [qx, qy, qz]
+#
+#     Returns:
+#         S = skew symmetric matrix built using q
+#     """
+#     S = cs.SX.zeros(3, 3)
+#     S[0, 1] = -q[2]; S[0, 2] = q[1]
+#     S[1, 0] = q[2];  S[1, 2] = -q[0]
+#     S[2, 0] = -q[1]; S[2, 1] = q[0]
+#     return S
 
 def quaterion_product(q, p):
     """
@@ -70,7 +70,7 @@ def quaterion_product(q, p):
     q0 = q[3]
     p0 = p[3]
 
-    return [q0*p[0:3] + p0*q[0:3] + cs.mtimes(skew(q[0:3]), p[0:3]), q0*p0 - cs.mtimes(q[0:3].T, p[0:3])]
+    return [q0*p[0:3] + p0*q[0:3] + cs.mtimes(cs.skew(q[0:3]), p[0:3]), q0*p0 - cs.mtimes(q[0:3].T, p[0:3])]
 
 def toRot(q):
     """
@@ -82,22 +82,23 @@ def toRot(q):
         R: rotation matrix
 
     """
+
     R = cs.SX.zeros(3, 3)
     qi = q[0]; qj = q[1]; qk = q[2]; qr = q[3]
-    R[0, 0] = 1. - 2. * (qj * qj + qk * qk);
-    R[0, 1] = 2. * (qi * qj - qk * qr);
+    R[0, 0] = 1. - 2. * (qj * qj + qk * qk)
+    R[0, 1] = 2. * (qi * qj - qk * qr)
     R[0, 2] = 2. * (qi * qk + qj * qr)
-    R[1, 0] = 2. * (qi * qj + qk * qr);
-    R[1, 1] = 1. - 2. * (qi * qi + qk * qk);
+    R[1, 0] = 2. * (qi * qj + qk * qr)
+    R[1, 1] = 1. - 2. * (qi * qi + qk * qk)
     R[1, 2] = 2. * (qj * qk - qi * qr)
-    R[2, 0] = 2. * (qi * qk - qj * qr);
-    R[2, 1] = 2. * (qj * qk + qi * qr);
+    R[2, 0] = 2. * (qi * qk - qj * qr)
+    R[2, 1] = 2. * (qj * qk + qi * qr)
     R[2, 2] = 1. - 2. * (qi * qi + qj * qj)
 
     return R
 
 
-def double_integrator_with_floating_base(q, ndot, nddot, base_velocity_reference_frame = cas_kin_dyn.CasadiKinDyn.LOCAL):
+def double_integrator_with_floating_base(q, qdot, qddot, base_velocity_reference_frame = cas_kin_dyn.CasadiKinDyn.LOCAL):
     """
     Construct the floating-base dynamic model:
                 x = [q, ndot]
@@ -109,9 +110,9 @@ def double_integrator_with_floating_base(q, ndot, nddot, base_velocity_reference
     else if base_velocity_reference_frame = cas_kin_dyn.CasadiKinDyn.LOCAL_WORLD_ALIGNED
         linear and angular velocities expressed in WORLD coordinates.
     Args:
-        q: joint space coordinates: q = [x y z px py pz pw qj], where p is a quaternion
-        ndot: joint space velocities: ndot = [vx vy vz wx wy wz qdotj]
-        nddot: joint space acceleration: nddot = [ax ay ax wdotx wdoty wdotz qddotj]
+        q_sx: joint space coordinates: q = [x y z px py pz pw qj], where p is a quaternion
+        qdot_sx: joint space velocities: ndot = [vx vy vz wx wy wz qdotj]
+        qddot_sx: joint space acceleration: nddot = [ax ay ax wdotx wdoty wdotz qddotj]
 
     Returns:
         x: state x = [q, ndot]
@@ -119,38 +120,47 @@ def double_integrator_with_floating_base(q, ndot, nddot, base_velocity_reference
     """
     if base_velocity_reference_frame != cas_kin_dyn.CasadiKinDyn.LOCAL and base_velocity_reference_frame != cas_kin_dyn.CasadiKinDyn.LOCAL_WORLD_ALIGNED:
         raise Exception(f'base_velocity_reference_frame can be only LOCAL or LOCAL_WORLD_ALIGNED!')
+    # q, ndot, nddot
+
+    q_sx = cs.SX.sym('q_sx', q.shape[0])
+    qdot_sx = cs.SX.sym('ndot_sx', qdot.shape[0])
+    qddot_sx = cs.SX.sym('nddot_sx', qddot.shape[0])
+
 
     qw = cs.SX.zeros(4,1)
-    qw[0:3] = 0.5*ndot[3:6]
+    qw[0:3] = 0.5 * qdot_sx[3:6]
 
     if base_velocity_reference_frame == cas_kin_dyn.CasadiKinDyn.LOCAL:
-        if q.shape[1] == 1:
-            quaterniondot = quaterion_product(q[3:7], qw)
+        if q_sx.shape[1] == 1:
+            quaterniondot = quaterion_product(q_sx[3:7], qw)
         else:
-            quaterniondot = quaterion_product(q[3:7, :], qw)
+            quaterniondot = quaterion_product(q_sx[3:7, :], qw)
     else:
-        if q.shape[1] == 1:
-            quaterniondot = quaterion_product(qw, q[3:7])
+        if q_sx.shape[1] == 1:
+            quaterniondot = quaterion_product(qw, q_sx[3:7])
         else:
-            quaterniondot = quaterion_product(qw, q[3:7, :])
+            quaterniondot = quaterion_product(qw, q_sx[3:7, :])
 
     R = toRot([0., 0., 0., 1.])
     if base_velocity_reference_frame == cas_kin_dyn.CasadiKinDyn.LOCAL:
-        R = toRot(q[3:7])
-    x = cs.vertcat(q, ndot)
+        R = toRot(q_sx[3:7])
+    x = cs.vertcat(q_sx, qdot_sx)
 
-    if ndot.shape[1] == 1:
-        first = cs.mtimes(R, ndot[0:3])
+    if qdot_sx.shape[1] == 1:
+        first = cs.mtimes(R, qdot_sx[0:3])
     else:
-        first = cs.mtimes(R, ndot[0:3, :])
+        first = cs.mtimes(R, qdot_sx[0:3, :])
 
-    if ndot.shape[1] == 1:
-        third = ndot[6:ndot.shape[0]]
+    if qdot_sx.shape[1] == 1:
+        third = qdot_sx[6:qdot_sx.shape[0]]
     else:
-        third = ndot[6:ndot.shape[0], :]
+        third = qdot_sx[6:qdot_sx.shape[0], :]
 
-    xdot = cs.vertcat(first, cs.vertcat(*quaterniondot), third, nddot)
+    xdot = cs.vertcat(first, cs.vertcat(*quaterniondot), third, qddot_sx)
 
+    fun_sx = cs.Function('double_integrator_with_floating_base', [q_sx, qdot_sx, qddot_sx], [x, xdot])
+
+    x, xdot = fun_sx(q, qdot, qddot)
 
     return x, xdot
 
