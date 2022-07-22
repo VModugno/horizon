0968f03ef4a7568d6197f1ad15ef8872316d7b6c
EdoardoRomiti <edoardo.romiti@iit.it>
2022-07-22 12:55:49 +0200

    good example with ilqr

--- horizon/playground/sps_collision.py
+++ horizon/playground/sps_collision.py
@@ -7,7 +7,7 @@ from horizon.ros.replay_trajectory import replay_trajectory
 import os, time
 import numpy as np
 import casadi as cs
-import rospkg
+import rospkg, rospy
 
 from srdfdom.srdf import SRDF
 
@@ -15,7 +15,7 @@ from srdfdom.srdf import SRDF
 solver_type = 'ilqr'
 transcription_method = 'multiple_shooting'
 transcription_opts = dict(integrator='RK4')
-tf = 10.0
+tf = 4.0
 n_nodes = 100
 dt = tf / n_nodes
 
@@ -27,6 +27,23 @@ urdfpath = os.path.join(pkgpath, 'urdf', 'meca_500_r3_capsule.urdf.xacro')
 srdfpath = os.path.join(pkgpath, 'srdf', 'meca_500_r3_capsule.srdf')
 
 urdf = open(urdfpath, 'r').read()
+rospy.set_param('robot_description', urdf)
+
+# remove spheres from URDF
+from urdf_parser_py import urdf as urdf_parser
+robot = urdf_parser.Robot.from_xml_string(urdf)
+for link in robot.links:
+    if link.collisions:
+        spheres = []
+        for coll in link.collisions:
+            if isinstance(coll.geometry, urdf_parser.Sphere):
+                spheres.append(coll)
+        for sphere in spheres:
+            # link.collisions.remove(sphere)  # DOESN'T WORK!!!
+            link.remove_aggregate(sphere)
+
+urdf = robot.to_xml_string()
+
 kindyn = cas_kin_dyn.CasadiKinDyn(urdf)
 nq = kindyn.nq()
 nv = kindyn.nv()
@@ -37,7 +54,7 @@ if 'universe' in joint_names: joint_names.remove('universe')
 if 'floating_base_joint' in joint_names: joint_names.remove('floating_base_joint')
 
 # define dynamics
-prb = problem.Problem(n_nodes)
+prb = problem.Problem(n_nodes) #, casadi_type=cs.SX)
 q = prb.createStateVariable('q', nq)
 q_dot = prb.createStateVariable('q_dot', nv)
 q_ddot = prb.createInputVariable('q_ddot', nv)
@@ -81,8 +98,8 @@ p_start_B = FK_B(q=q_init)['ee_pos']
 # v_A = DFK_A(q=q, qdot=q_dot)['ee_vel_linear']
 # v_B = DFK_B(q=q, qdot=q_dot)['ee_vel_linear']
 
-p_tgt_A = p_start_A + np.array([0.3, -0.1, 0.0])
-p_tgt_B = p_start_B + np.array([-0.1, +0.1, 0.0])
+p_tgt_A = p_start_A + np.array([0.6, -0.0, 0.0])
+p_tgt_B = p_start_B + np.array([-0.1, +0.0, 0.0])
 
 # final node constraint velocity
 final_node=n_nodes
@@ -92,7 +109,7 @@ q_dot.setBounds(lb=np.zeros(nv), ub=np.zeros(nv), nodes=final_node)
 
 # collision
 srdf = open(srdfpath, 'r').read()
-use_fcl_wrapper = False
+use_fcl_wrapper = True
 if use_fcl_wrapper:
     ch = cas_kin_dyn.CollisionHandler(kd=kindyn, srdf=srdf)
     collision_fn = ch.getDistanceFunction()
@@ -105,20 +122,28 @@ else:
 #                            ub=np.full(collision_fn.size1_out(0), np.inf))
 
 collision_sx = collision_fn(q)
-collision_barrier = cs.if_else(collision_sx > 0, 0, collision_sx)
-coll_weight = prb.createParameter('coll_weight', dim=1)
-prb.createResidual('collision', coll_weight*collision_barrier)
-coll_weight.assign(1e2)
+print(collision_sx.size1())
+
+if solver_type == 'ilqr':
+    coll_threshold = prb.createParameter('coll_threshold', 1)
+    coll_threshold.assign(0.10)
+    collision_barrier = cs.if_else(collision_sx > coll_threshold, 0, collision_sx - coll_threshold)
+    coll_weight = prb.createParameter('coll_weight', dim=1)
+    prb.createResidual('collision', coll_weight*collision_barrier)
+    coll_weight.assign(1e2)
+else:
+    collision_const = prb.createConstraint('collision_const', collision_sx)
+    collision_const.setBounds(lb=np.zeros(collision_sx.size1()), ub=np.inf*np.ones(collision_sx.size1()))
 
 
 ##### Cost function #####
 # penalize input
-prb.createIntermediateResidual('min_u', np.sqrt(1) * q_ddot)
+prb.createIntermediateResidual('min_u', np.sqrt(1e-1) * q_ddot)
 
 # penalize joint velocity
-prb.createIntermediateResidual('min_qdot_linear', np.sqrt(1e4) * q_dot[0])
-prb.createIntermediateResidual('min_qdot_rotary', np.sqrt(1e4) * q_dot[7])
-# prb.createIntermediateResidual('min_qdot_rotary', np.sqrt(1e4) * q_dot)
+# prb.createIntermediateResidual('min_qdot_linear', np.sqrt(1e4) * q_dot[0])
+# prb.createIntermediateResidual('min_qdot_rotary', np.sqrt(1e4) * q_dot[7])
+prb.createIntermediateResidual('min_qdot', np.sqrt(1) * q_dot)
 
 # create solver
 prb_solver = solver.Solver.make_solver(
@@ -126,11 +151,15 @@ prb_solver = solver.Solver.make_solver(
     prb,
     opts={
         'ipopt.tol': 1e-4,
-        'ipopt.max_iter': 2000,
+        'ipopt.max_iter': 200,
+        'ipopt.hessian_approximation': 'limited-memory',
+        'ipopt.linear_solver': 'ma57',
         'ilqr.codegen_enabled': True,
         'ilqr.codegen_workdir': '/home/edoardo/ilqr_codegen',
+        'ilqr.alpha_min': 0.01,
         'ilqr.enable_gn': True,
         'ilqr.max_iter': 400,
+        'ilqr.verbose': True,
         }
 )
 
@@ -142,6 +171,17 @@ except:
 # solver
 tic = time.time()
 prb_solver.solve()
+
+prb.getState().setInitialGuess(prb_solver.x_opt)
+prb.getInput().setInitialGuess(prb_solver.u_opt)
+coll_threshold.assign(0.05)
+prb_solver.solve()
+
+prb.getState().setInitialGuess(prb_solver.x_opt)
+prb.getInput().setInitialGuess(prb_solver.u_opt)
+coll_threshold.assign(0.01)
+prb_solver.solve()
+
 toc = time.time()
 print('time elapsed solving:', toc - tic)
 
