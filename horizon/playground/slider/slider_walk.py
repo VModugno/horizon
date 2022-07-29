10239694a5edb0f5ba0ee026e35775ca628ef65c
FrancescoRuscelli <francescoruscelli24@gmail.com>
2022-07-29 13:00:02 +0200

    updated slider walk

--- horizon/playground/slider/slider_walk.py
+++ horizon/playground/slider/slider_walk.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
 from horizon.rhc.taskInterface import TaskInterface
+from horizon.problem import Problem
+from horizon.rhc.model_description import *
 from horizon.rhc.tasks.interactionTask import InteractionTask
 from horizon.utils.actionManager import ActionManager, Step
 from casadi_kin_dyn import py3casadi_kin_dyn
@@ -37,14 +39,61 @@ q_init = {"Left_Roll":         0.,
           "Right_Foot_Roll":   0.,
          }
 
-problem_opts = {'ns': ns, 'tf': tf, 'dt': dt}
+# set up model description
+urdf = urdf.replace('continuous', 'revolute')
+fixed_joints = []
+fixed_joints_pos = [q_init[k] for k in fixed_joints]
+fixed_joint_map = {k: q_init[k] for k in fixed_joints}
+q_init = {k: v for k, v in q_init.items() if k not in fixed_joints}
+kd = py3casadi_kin_dyn.CasadiKinDyn(urdf, fixed_joints=fixed_joint_map)
 
-model_description = 'whole_body'
-ti = TaskInterface(urdf,
-        q_init, base_init,
-        problem_opts,
-        model_description,
-        is_receding=True)
+contact_dict = {
+    'Left_Foot': {
+        'type': 'vertex',
+        'vertex_frames': [
+            'Left_Foot_A',
+            'Left_Foot_B',
+            'Left_Foot_C',
+            'Left_Foot_D',
+        ]
+    },
+
+    'Right_Foot': {
+        'type': 'vertex',
+        'vertex_frames': [
+            'Right_Foot_A',
+            'Right_Foot_B',
+            'Right_Foot_C',
+            'Right_Foot_D',
+        ]
+    }
+}
+
+# set up problem
+N = 50
+tf = 10.0
+dt = tf / N
+
+prb = Problem(N, receding=True, casadi_type=cs.MX)
+prb.setDt(dt)
+
+# set up model
+model = FullModelInverseDynamics(problem=prb,
+                                 kd=kd,
+                                 q_init=q_init,
+                                 base_init=base_init)
+# else:
+#     model = SingleRigidBodyDynamicsModel(problem=prb,
+#                                          kd=kd,
+#                                          q_init=q_init,
+#                                          base_init=base_init,
+#                                          contact_dict=contact_dict)
+
+
+
+
+ti = TaskInterface(prb=prb,
+                   model=model)
 
 ti.setTaskFromYaml(os.path.dirname(__file__) + '/slider_config.yaml')
 
@@ -80,17 +129,16 @@ q = ti.prb.getVariables('q')
 v = ti.prb.getVariables('v')
 a = ti.prb.getVariables('a')
 
-import casadi as cs
-cd_fun = ti.kd.computeCentroidalDynamics()
+cd_fun = ti.model.kd.computeCentroidalDynamics()
 
 # adding minimization of angular momentum
 # h_lin, h_ang, dh_lin, dh_ang = cd_fun(q, v, a)
 # ti.prb.createIntermediateResidual('min_angular_mom', 0.1 * dh_ang)
 
-q.setBounds(ti.q0, ti.q0, nodes=0)
-v.setBounds(ti.v0, ti.v0, nodes=0)
+q.setBounds(ti.model.q0, ti.model.q0, nodes=0)
+v.setBounds(ti.model.v0, ti.model.v0, nodes=0)
 
-q.setInitialGuess(ti.q0)
+q.setInitialGuess(ti.model.q0)
 
 for cname, cforces in ti.model.cmap.items():
     for c in cforces:
@@ -125,7 +173,7 @@ if replay_motion:
     # single replay
     q_sol = solution['q']
     frame_force_mapping = {cname: solution[f.getName()] for cname, f in ti.model.fmap.items()}
-    repl = replay_trajectory.replay_trajectory(dt, ti.kd.joint_names()[2:], q_sol, frame_force_mapping, ti.kd_frame, ti.kd)
+    repl = replay_trajectory.replay_trajectory(dt, ti.model.kd.joint_names()[2:], q_sol, frame_force_mapping, ti.model.kd_frame, ti.model.kd)
     repl.sleep(1.)
     repl.replay(is_floating_base=True)
 # =========================================================================
