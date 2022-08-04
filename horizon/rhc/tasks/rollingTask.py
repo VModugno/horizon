713c31e0373b9d37e0253ae32232aa4ea69f7c13
FrancescoRuscelli <francescoruscelli24@gmail.com>
2022-08-04 13:11:36 +0200

    some improvements in rolling

--- horizon/rhc/tasks/rollingTask.py
+++ horizon/rhc/tasks/rollingTask.py
@@ -23,8 +23,69 @@ class RollingTask(Task):
 
         self._initialize()
 
+    # def _acceleration_formulation(self):
 
-    def _initialize(self):
+        # q = self.prb.getVariables('q')
+        # v = self.prb.getVariables('v')
+        # a = self.prb.getVariables('a')
+
+        # dfk_distal = self.kin_dyn.frameVelocity(self.frame, self.kd_frame)
+        # ee_v_distal_t = dfk_distal(q=q, qdot=v)['ee_vel_linear']
+        # ee_v_distal_r = dfk_distal(q=q, qdot=v)['ee_vel_angular']
+
+        # ddfk_distal = self.kin_dyn.frameAcceleration(self.frame, self.kd_frame)
+        # ee_a_distal_t = ddfk_distal(q=q, qdot=v)['ee_acc_linear']
+        # ee_a_distal_r = ddfk_distal(q=q, qdot=v)['ee_acc_angular']
+
+        # normal of plane where rolling happens
+        # world_contact_plane_normal = np.array([0.0001, 0., 1.])
+
+        # rot velocity (in world) of wheel
+        # omega = ee_v_distal_r
+
+        # radius of wheel
+        # r = - world_contact_plane_normal * self.radius
+
+        # velocity of point contact between wheel and plane
+        # v_contact_point = ee_v_distal_t + cs.cross(omega, r)
+
+        # k = .1
+        # omega_a = ee_a_distal_r
+        # a_contact_point = ee_a_distal_t + cs.cross(omega_a, r) + k * v_contact_point
+
+    # def _trial_formulation(self):
+        #
+        # q = self.prb.getVariables('q')
+        # v = self.prb.getVariables('v')
+        #
+        # if self.frame == 'wheel_1':
+        #     index = 7+ 4
+        # elif self.frame == 'wheel_2':
+        #     index = 7 + 10
+        # elif self.frame == 'wheel_3':
+        #     index = 7 + 16
+        # elif self.frame == 'wheel_4':
+        #     index = 7 + 22
+        #
+        # ankle_yaw = q[index]
+        # contact_at_wheel_frame = self.frame.replace('wheel', 'contact')
+        #
+        # wheel_dfk_distal = self.kin_dyn.frameVelocity(self.frame, self.kd_frame)
+        # wheel_v_distal_t = wheel_dfk_distal(q=q, qdot=v)['ee_vel_linear']
+        # wheel_v_distal_r = wheel_dfk_distal(q=q, qdot=v)['ee_vel_angular']
+        #
+        # contact_dfk_distal = self.kin_dyn.frameVelocity(contact_at_wheel_frame, self.kd_frame)
+        # contact_v_distal_t = contact_dfk_distal(q=q, qdot=v)['ee_vel_linear']
+        # contact_v_distal_r = contact_dfk_distal(q=q, qdot=v)['ee_vel_angular']
+        #
+        #
+        # print(self.frame, "ankle:", ankle_yaw)
+        # fun = contact_v_distal_t[0] * cs.sin(ankle_yaw) - contact_v_distal_t[1] * cs.cos(ankle_yaw)
+
+        # return fun
+
+    def _velocity_formulation(self):
+
         q = self.prb.getVariables('q')
         v = self.prb.getVariables('v')
 
@@ -32,18 +93,25 @@ class RollingTask(Task):
         ee_v_distal_t = dfk_distal(q=q, qdot=v)['ee_vel_linear']
         ee_v_distal_r = dfk_distal(q=q, qdot=v)['ee_vel_angular']
 
-        world_contact_plane_normal = np.zeros(3)
-        world_contact_plane_normal[2] = 1.
+        # normal of plane where rolling happens
+        world_contact_plane_normal = np.array([0.0001, 0., 1.])
 
-        wheel_axis = np.zeros(3)
-        wheel_axis[2] = 1.
-
+        # rot velocity (in world) of wheel
         omega = ee_v_distal_r
+
+        # radius of wheel
         r = - world_contact_plane_normal * self.radius
 
+        # velocity of point contact between wheel and plane
         v_contact_point = ee_v_distal_t + cs.cross(omega, r)
 
-        self.constr = self.instantiator(f'{self.name}_rolling_task', self.weight * v_contact_point, nodes=self.nodes)
+        fun = v_contact_point
+        return fun
+
+    def _initialize(self):
+
+        fun = self._velocity_formulation()
+        self.constr = self.instantiator(f'{self.name}_rolling_task', self.weight * fun, nodes=self.nodes)
 
 
     def setNodes(self, nodes):
