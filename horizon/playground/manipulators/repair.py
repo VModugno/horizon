27639fd0d0b9f5df26600c7f57c3eb9399eb00fc
Francesco Ruscelli <francesco.ruscelli@iit.it>
2022-09-21 11:37:38 +0200

    resurrected repair

--- horizon/playground/manipulators/repair.py
+++ horizon/playground/manipulators/repair.py
@@ -1,5 +1,7 @@
+from horizon.problem import Problem
 from horizon.rhc.taskInterface import TaskInterface
 from horizon.rhc.tasks.cartesianTask import CartesianTask, Task
+from horizon.rhc.model_description import *
 from horizon.ros import replay_trajectory
 from horizon.solvers import Solver
 import rospkg, rospy
@@ -12,11 +14,17 @@ urdf_path = rospkg.RosPack().get_path('repair_urdf') + '/urdf/repair.urdf'
 urdf = open(urdf_path, 'r').read()
 rospy.set_param('/robot_description', urdf)
 
+solver_type = 'ipopt'  # ilqr
+transcription_method = 'multiple_shooting'  # can choose between 'multiple_shooting' and 'direct_collocation'
+transcription_opts = dict(integrator='RK4')  # integrator used by the multiple_shooting
+
+solver_opt = dict(type=solver_type)
 
 def add_cartesian_tasks_vel():
     cart_vel_1 = {'type': 'Cartesian',
                   'distal_link': 'arm_1_tcp',
                   'name': 'arm_1_tcp_ee_vel_world',
+                  'indices': [0, 1, 2, 3, 4, 5],
                   'indices': [0, 1, 2, 3, 4, 5],
                   'nodes': range(1, N),
                   'cartesian_type': 'velocity'}
@@ -70,17 +78,7 @@ def add_cartesian_tasks_pos():
     ee_cart_2.setRef(goal_vec_1)
 
 
-solver_type = 'ipopt'  # ilqr
-transcription_method = 'multiple_shooting'  # can choose between 'multiple_shooting' and 'direct_collocation'
-transcription_opts = dict(integrator='RK4')  # integrator used by the multiple_shooting
-
-N = 50
-tf = 2.0
-dt = tf / N
-nf = 3
-problem_opts = {'ns': N, 'tf': tf, 'dt': dt}
-
-model_description = 'whole_body'
+# robot model
 q_init = {}
 
 q_init[f'arm_1_joint_1'] = 0.
@@ -98,10 +96,29 @@ q_init[f'arm_2_joint_5'] = 0.
 q_init[f'arm_2_joint_6'] = -0.4
 q_init[f'arm_2_joint_7'] = 0.
 
-ti = TaskInterface(urdf, q_init, None, problem_opts, model_description, is_receding=False)
+kd = pycasadi_kin_dyn.CasadiKinDyn(urdf)
+kd_frame = pycasadi_kin_dyn.CasadiKinDyn.LOCAL_WORLD_ALIGNED
+# joint_names = kd.joint_names()[2:]
+
+N = 50
+tf = 2.0
+dt = tf / N
+nf = 3
+
+prb = Problem(N, receding=False)  # logging_level=logging.DEBUG
+prb.setDt(dt)
+
+# set up model
+model = FullModelInverseDynamics(problem=prb,
+                                 kd=kd,
+                                 q_init=q_init,
+                                 floating_base=False)
+
+ti = TaskInterface(prb, model)
 
 # cartesian_task
 add_cartesian_tasks_vel()
+
 
 q = ti.prb.getVariables('q')
 v = ti.prb.getVariables('v')
@@ -110,15 +127,16 @@ a = ti.prb.getVariables('a')
 # TODO: being the only cost, why changing its weight changes also the solution??
 ti.prb.createIntermediateResidual('min_q', a)
 
-q.setBounds(ti.q0, ti.q0, nodes=0)
-v.setBounds(ti.v0, ti.v0, nodes=0)
+q.setBounds(model.q0, model.q0, nodes=0)
+v.setBounds(model.v0, model.v0, nodes=0)
 
-q.setInitialGuess(ti.q0)
+q.setInitialGuess(model.q0)
 
-
+ti.setSolverOptions(solver_opt)
 ti.finalize()
 ti.bootstrap()
 solution = ti.solution
+
 # ===========================================================================================
 # ================================== to wrap ================================================
 # ===========================================================================================
@@ -132,7 +150,7 @@ solution = ti.solution
 
 
 q_sol = solution['q']
-repl = replay_trajectory.replay_trajectory(dt, ti.kd.joint_names()[1:], q_sol)
+repl = replay_trajectory.replay_trajectory(dt, kd.joint_names()[1:], q_sol)
 repl.sleep(1.)
 repl.replay(is_floating_base=False)
 
