7e0b8ca778ed90645bb646a9f93d7a020f6067f5
Francesco Ruscelli <francesco.ruscelli@iit.it>
2022-11-21 18:47:38 +0100

    nothing much

--- horizon/rhc/action_manager/spot_action_man_pybind.py
+++ horizon/rhc/action_manager/spot_action_man_pybind.py
@@ -188,7 +188,7 @@ for c in contacts:
     c_phases[c].addPhase(stance)
     c_phases[c].addPhase(stance)
     c_phases[c].addPhase(stance)
-    # c_phases[c].addPhase(flight)
+    c_phases[c].addPhase(flight)
     c_phases[c].addPhase(stance)
     c_phases[c].addPhase(stance)
     c_phases[c].addPhase(stance)
@@ -199,19 +199,20 @@ for c in contacts:
     c_phases[c].addPhase(stance)
 
 
-
+range_trot_1 = range(4, 16, 2)
+range_trot_2 = range(5, 17, 2)
 
 lift_contact = 'lh_foot'
-for i in range(4, 16, 2):
+for i in range_trot_1:
     c_phases[lift_contact].addPhase(c_phases[lift_contact].getRegisteredPhase(f'flight_{lift_contact}'), i)
 lift_contact = 'rh_foot'
-for i in range(5, 17, 2):
+for i in range_trot_2:
     c_phases[lift_contact].addPhase(c_phases[lift_contact].getRegisteredPhase(f'flight_{lift_contact}'), i)
 lift_contact = 'lf_foot'
-for i in range(5, 17, 2):
+for i in range_trot_2:
     c_phases[lift_contact].addPhase(c_phases[lift_contact].getRegisteredPhase(f'flight_{lift_contact}'), i)
 lift_contact = 'rf_foot'
-for i in range(4, 16, 2):
+for i in range_trot_1:
     c_phases[lift_contact].addPhase(c_phases[lift_contact].getRegisteredPhase(f'flight_{lift_contact}'), i)
 
 # for name, timeline in c_phases.items():
